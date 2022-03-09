d11c31c8eb23fba8676c97cd37209b2b9c1f2d3a
annashiii <bhogan.gths@gmail.com>
2022-03-08 21:11:20 -0500

    Prompt user if no tasks present

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import ToDo from './Todo'
-
+import { Heading } from '@chakra-ui/react';
 
 export default function TodoList({toDoList, handleToggle}) {
     return(
         <div className='list'>
-            {toDoList.map(todo => {
+            {toDoList.length > 0 ? 
+            toDoList.map(todo => {
                 return(
                     <ToDo todo={todo} handleToggle={handleToggle}/>
                 )
-            })}
+            }) : <Heading fontFamily='Helvetica' pl='40px' pt='100px' color='gray.500' size='md'>Add a task to get started</Heading>}
             
         </div>
     );
